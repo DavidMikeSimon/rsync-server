b6f11ca3c0140cc880153705a301cd23f15fc675
David Simon <david.simon@color.com>
2023-07-04 20:34:09 -0400

    Preserve executability

--- rsync-backup.sh
+++ rsync-backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo rsync_backup_time $(date +%s) > ~/rsync-backup-time.prom
-rsync -rltv --delete -e "ssh -p 2222" ~/rsync-backup-time.prom ~/STUFF-A ~/STUFF-B USER@SERVER:/backups/PLACE/
+rsync -rltEv --delete -e "ssh -p 2222" ~/rsync-backup-time.prom ~/STUFF-A ~/STUFF-B USER@SERVER:/backups/PLACE/
 rm ~/rsync-backup-time.prom
